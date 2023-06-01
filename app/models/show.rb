9c22f32602450e2d7db1c604b2cd358239db9480
kush Muhamad <kushmohammed98@yahoo.com>
2023-06-01 14:06:10 +0300

    completed task

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,19 @@
 class Show < ActiveRecord::Base
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
   
-end
+    def characters_list
+      characters.map(&:name)
+    end
+  
+    def network
+        Network.find(network_id)
+      
+    end
+  
+    def actors_list
+      actors.map(&:full_name)
+    end
+  end
+  
